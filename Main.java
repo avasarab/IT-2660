24c4ba7cf528ca37032e2e787ff5813769ccaf48
avasarab <14avasarab@gmail.com>
2025-11-24 21:33:56 -0500

    Implement course credit hours management in Main.java
    This implementation demonstrates the use of MyMap to store and manipulate course credit hours.

---
+++ Main.java
@@ -0,0 +1,34 @@
+class Main {
+  public static void main(String[] args) {
+    // Use the instructions in Blackboard or instructions.md to complete Lab 6
+        MyMap<String, Integer> creditHours = new MyHashMap<>();
+
+        creditHours.put("IT-1025", 3);
+        creditHours.put("IT-1050", 3);
+        creditHours.put("IT-1150", 3);
+        creditHours.put("IT-2310", 3);
+        creditHours.put("IT-2320", 4);
+        creditHours.put("IT-2351", 4);
+        creditHours.put("IT-2650", 4);
+        creditHours.put("IT-2660", 4);
+        creditHours.put("IT-2030", 4);
+
+
+        System.out.println("Contains IT-1025? " + creditHours.containsKey("IT-1025"));
+        System.out.println("Contains IT-2110? " + creditHours.containsKey("IT-2110"));
+
+
+        System.out.println("\nAll courses and credit hours:");
+        for (MyMap.Entry<String, Integer> entry : creditHours.entrySet()) {
+            System.out.println(entry.getKey() + ", " + entry.getValue());
+        }
+ 
+        creditHours.remove("IT-2030");
+        creditHours.remove("IT-1150");
+
+        System.out.println("\nRemaining credit hours:");
+        for (Integer value : creditHours.values()) {
+            System.out.println(value);
+        }
+    }
+}
